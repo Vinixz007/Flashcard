a33fefc4ec59c23b0ac58092ee2c1a1f68c6436d
Vinixz007 <148581003+Vinixz007@users.noreply.github.com>
2024-11-05 11:49:06 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,35 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
-
-criaCartao(
-    'Geografia',
-    'Qual é a captal da França?',
-    'A captal da França é Paris'
-)
-
-criaCartao(
-    'Programação',
-    'O que é uma função',
-    'Uma função é um bloco de código que executa alguma tarefa'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em Inglês é HI (RAI)'
+    'Biologia',
+    'O que é a primeira lei de Mendel?',
+    'Todas as características de um indivíduosão determinadas por genes que separam-se, durante a formação dos gametas, sendo que, assim, pai e mãetransmitem apenas um gene para seus descendentes.'
 )
 
 criaCartao(
     'Biologia',
-    'Quem é considerado o pai da genética?',
-    'Gregor Mendel'
+    'Quais são as 3 leis de Mendel?',
+    'lei da segregação, a lei da distribuição independente e a lei da dominância.'
 )
 
 criaCartao(
-    'Matemática',
-    'Qual é o valor de Pi?',
-    '3,14'
+    'Biologia',
+    'Quais são as leis de lamarck?',
+    'Lei da herança dos caracteres adquiridos”, e à “Lei do uso e desuso”'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a definição de um gene?',
+    'O gene é um segmento de uma molécula de DNA (ácido desoxirribonucleico) responsável pelas características herdadas geneticamente.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a diferença entre um gene dominante e um gene recessivo?',
+    'Se a expressão de um traço requer apenas uma cópia do gene (um alelo), este traço é considerado dominante. Se a expressão de um traço requer 2 cópias de um gene (2 alelos), este traço é considerado recessivo.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é terapia gênica?',
+    'A terapia gênica visa diminuir ou evitar o avanço de doenças graves e neurodegenerativas, como a doença de Parkinson.'
 )
